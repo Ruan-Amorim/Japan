b21f6294a1541431d20b62e96e044479a68bea50
Ruan Amorim de Mendonça <150949063+Ruan-Amorim@users.noreply.github.com>
2024-11-14 20:30:42 -0300

    add index of project

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data_regioes = [{
+    {
+        "name": "Hokkaido",
+    },
+    {
+        "name": "Tohoku",
+    },
+    {
+        "name": "Kanto",
+    },
+    {
+        "name": "Chubu",
+    },
+    {
+        "name": "Kansai",
+    },
+    {
+        "name": "Chugoku",
+    },
+    {
+        "name": "Shikoku",
+    },
+    {
+        "name": "Kyushu e Okinawa",
+    },
+}];
